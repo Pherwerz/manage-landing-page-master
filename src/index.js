3a1da2298f7ac1ef8252445f42600b83a341609d
pherwerz <lordrisk101@gmail.com>
2020-06-27 22:57:10 +0100

    final commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ let tracker = true;
 const button = document.querySelector('.header__nav');
 const nav = document.querySelector('.navigation');
 const image = document.querySelector('#img');
+const slides = Array.from(document.querySelectorAll('.review__box'));
+const dots = Array.from(document.querySelectorAll('.review__dots'));
+let slideIndex = 0;
 
 button.addEventListener('click', () => {
   if (tracker) {
@@ -18,3 +21,18 @@ button.addEventListener('click', () => {
     tracker = true;
   }
 });
+
+const show = () => {
+  slides.forEach((cur, i) => {
+    cur.style.display = 'none';
+    dots[i].classList.remove('review__dots--active');
+  });
+
+  slides[slideIndex].style.display = 'block';
+  dots[slideIndex].classList.add('review__dots--active');
+  slideIndex++;
+
+  if (slideIndex === 4) slideIndex = 0;
+};
+
+if (window.innerWidth <= 600) setInterval(show, 2000);
