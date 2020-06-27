f798a0ee502746b6d9e5d0b966a7238eb3aa099c
pherwerz <lordrisk101@gmail.com>
2020-06-27 19:11:07 +0100

    lastest commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './index.scss';
+
+let tracker = true;
+const button = document.querySelector('.header__nav');
+const nav = document.querySelector('.navigation');
+const image = document.querySelector('#img');
+
+button.addEventListener('click', () => {
+  if (tracker) {
+    image.src = './img/icon-close.svg';
+    nav.style.visibility = 'visible';
+    nav.style.opacity = '1';
+    tracker = false;
+  } else {
+    image.src = './img/icon-hamburger.svg';
+    nav.style.visibility = 'hidden';
+    nav.style.opacity = '0';
+    tracker = true;
+  }
+});
